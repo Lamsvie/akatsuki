19477b727a50037548d14e5b059482de26b4bfbd
Mouhamed Sarr <msarr2101@gmail.com>
2022-07-07 10:43:58 +0000

    Ceci est le push de MS

---
+++ akatsuki/akatsuki/asia.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div>
+        <img src="1.jpg"/>
+    </div>
+    <div>
+        <img src="2.jpg"/>
+    </div>
+    <div>
+        
+        <img src="3.jpg"/>
+    </div>
+</body>
+</html>
